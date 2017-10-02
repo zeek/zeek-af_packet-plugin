f385ca53c4804e98041090f420686e40b1e3162b
Jan Grashoefer <jan.grashoefer@gmail.com>
2017-10-03 00:03:51 +0200

    Use fallback kernel headers.
    If the kernel headers for the running kernel cannot be found, fallback
    to the newest available kernel headers.

--- cmake/FindKernelHeaders.cmake
+++ cmake/FindKernelHeaders.cmake
@@ -13,6 +13,12 @@
 #
 #  KERNELHEADERS_FOUND         System has kernel headers.
 
+# Search paths:
+set(KERNEL_PATHS
+  /usr/src/linux-headers-
+  /usr/src/kernels/
+)
+
 # Find the kernel release
 execute_process(
   COMMAND uname -r
@@ -20,11 +26,41 @@ execute_process(
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+foreach ( KERNEL_PATH ${KERNEL_PATHS} )
+	list(APPEND KERNEL_RELEASE_PATHS "${KERNEL_PATH}${KERNEL_RELEASE}")
+endforeach (KERNEL_PATH)
+
 find_path(KERNELHEADERS_ROOT_DIR
   NAMES include/linux/user.h
-  PATHS /usr/src/linux-headers-${KERNEL_RELEASE}
-        /usr/src/kernels/${KERNEL_RELEASE}
+  PATHS ${KERNEL_RELEASE_PATHS}
 )
+
+# Fallback to non-release header versions
+if ( NOT KERNELHEADERS_ROOT_DIR )
+	foreach ( KERNEL_PATH ${KERNEL_PATHS} )
+		# Search for any installed headers
+		file(GLOB KERNELS_AVAILABLE "${KERNEL_PATH}*")
+
+		if ( KERNELS_AVAILABLE )
+			# Get the newest kernel
+			list(SORT KERNELS_AVAILABLE)
+			list(REVERSE KERNELS_AVAILABLE)
+			list(GET KERNELS_AVAILABLE 0 KERNEL_FALLBACK)
+
+			find_path(KERNELHEADERS_ROOT_DIR
+			  NAMES include/linux/user.h
+			  PATHS ${KERNEL_FALLBACK}
+			)
+
+			if ( KERNELHEADERS_ROOT_DIR )
+				message(WARNING "No headers found that match the running "
+				  "kernel version: Using fallback kernel headers instead."
+				)
+				break ()
+			endif ()
+		endif ()
+	endforeach ( KERNEL_PATH )
+endif ()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(KernelHeaders DEFAULT_MSG
