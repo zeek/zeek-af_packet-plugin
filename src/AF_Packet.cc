9178e5a313d789cf38e51c1f2dafda881b942a45
Jan Grashoefer <jan.grashoefer@gmail.com>
2020-09-25 18:42:47 +0200

    Hacked VLAN support.

--- src/AF_Packet.cc
+++ src/AF_Packet.cc
@@ -227,6 +227,9 @@ bool AF_PacketSource::ExtractNextPacket(Packet* pkt)
 
 		pkt->Init(props.link_type, &current_hdr.ts, current_hdr.caplen, current_hdr.len, data);
 
+		if ( packet->tp_status & TP_STATUS_VLAN_VALID )
+			pkt->vlan = packet->hv1.tp_vlan_tci;
+
 		if ( current_hdr.len == 0 || current_hdr.caplen == 0 )
 			{
 			Weird("empty_af_packet_header", pkt);
